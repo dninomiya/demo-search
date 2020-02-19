7a66609955fc50c0241d8030c838201e0327a855
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-02-19 11:55:22 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import * as algoliasearch from 'algoliasearch/lite/';
+import {
+  InstantSearchConfig,
+  SearchClient,
+  SearchParameters
+} from 'angular-instantsearch/instantsearch/instantsearch';
+
+const searchClient = algoliasearch(
+  'CQ04MZASL7',
+  '5caf57ddaea6361a3d2edf12ab3167e4'
+);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-search';
+  inputParams: SearchParameters = {
+    hitsPerPage: 5
+  };
+
+  resultParams = {
+    hitsPerPage: 5,
+    page: 0,
+    query: 'リ'
+  };
+
+  config = {
+    indexName: 'pokemons',
+    searchClient
+  };
+
+  search(value) {
+    const url = location.href;
+    console.log(url + '?name=' + value);
+  }
+
+  nextPage() {
+    this.resultParams.page++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgAisModule } from 'angular-instantsearch';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatAutocompleteModule,
+  MatFormFieldModule,
+  MatIconModule
+} from '@angular/material';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchInputComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgAisModule.forRoot(),
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
